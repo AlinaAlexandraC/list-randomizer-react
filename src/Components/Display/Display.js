162e6a716ae3048c9838dd30b94ac98ac0e18fcf
AlinaAlexandraC <alina_alexxa97b@yahoo.com>
2025-02-03 18:26:29 +0200

    responsive

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -16,8 +16,7 @@ const Display = () => {
             <div className="display-title">
                 The internet chose this title:
             </div>
-            <div className="anime-title-container">
-            </div>
+            <div className="anime-picture-container"></div>
             <div className="anime-title">{pickedTitle}</div>
             <div className="get-title-button btn" onClick={randomizeTitle}>Get Title</div>
         </div>
